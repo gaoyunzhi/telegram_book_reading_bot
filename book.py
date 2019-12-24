7aa3046b79c009e7a21e03a4bdded5741156da3a
Yunzhi Gao <gaoyunzhi@gmail.com>
2019-12-24 10:04:43 -0800

    commit

--- book.py
+++ book.py
@@ -10,7 +10,8 @@ import random
 with open('CREDENTIALS') as f:
 	CREDENTIALS = yaml.load(f, Loader=yaml.FullLoader)
 tele = Updater(CREDENTIALS['bot_token'], use_context=True)
-channel = tele.bot.get_chat(-1001435357912)
+chat_id = -1001435357912
+channel = tele.bot.get_chat(chat_id)
 
 books = []
 for filename in glob.glob("books/*.txt"):
@@ -25,8 +26,34 @@ def retry(f):
 		time.sleep(random.random() * 100)
 		retry(f)
 
-for filename, book in books:
-	channel.send_message(filename)
-	for sentence in book.split('\n\n'):
-		time.sleep(random.random() * 6)
-		retry(lambda: channel.send_message(sentence))
+def getBookName(filename):
+	return fileanme[6:-4]
+
+def getNewInfo(filename, message_id):
+	book_name = getBookName(filename)
+	link = 't.me/%s/%d' % (channel.username, message_id)
+	return '[%s](%s)' % (book_name, link)
+
+def getInfo(desc):
+	r = []
+	for k, v in desc:
+		r.append(getNewInfo(k, v))
+	r.sort()
+	return '\n'.append(r)
+
+def sending():
+	desc = {}
+	for filename, book in books:
+		message_id = channel.send_message(filename).message_id
+		desc[filename] = message_id
+		channel = tele.bot.get_chat(chat_id)
+		tele.bot.set_chat_description(chat_id, 
+			channel.description + '\n' + getNewInfo(filename, message_id), parse_mode='Markdown')
+		for sentence in book.split('\n\n')[:5]: # testing
+			time.sleep(random.random() * 6)
+			retry(lambda: channel.send_message(sentence))
+		pin_id = channel.send_message(
+			'书籍电梯:\n\n' + getInfo(desc), parse_mode='Markdown', disable_web_page_preview=True).message_id
+		tele.bot.pin_chat_message(chat_id, pin_id)
+
+sending()
