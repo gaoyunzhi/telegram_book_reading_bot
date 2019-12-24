54ae481c3b039d5d179f5fa7790bebc89c49314e
Yunzhi Gao <gaoyunzhi@gmail.com>
2019-12-24 09:20:11 -0800

    read txt

--- book.py
+++ book.py
@@ -3,9 +3,16 @@
 
 from telegram.ext import Updater
 import yaml
+import glob
 
 with open('CREDENTIALS') as f:
 	CREDENTIALS = yaml.load(f, Loader=yaml.FullLoader)
 tele = Updater(CREDENTIALS['bot_token'], use_context=True)
 channel = tele.bot.get_chat(-1001435357912)
 
+books = []
+for filenames in glob.glob("books/*.txt"):
+	with open(filename) as f:
+		books.append(filename, f.read())
+
+ 
